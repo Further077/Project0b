9263022c05f18d8734a11360e2d25fbe95794c42
Ron Giambalvo <further077@gmail.com>
2012-09-05 21:26:09 -0500

    clean files

---
+++ TestIterator.java
@@ -0,0 +1,108 @@
+package cs271.lab.list;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestIterator {
+
+	private List<Integer> list;
+
+	@Before
+	public void setUp() throws Exception {
+		list = new ArrayList<Integer>();
+		// TODO also try with a LinkedList - does it make any difference?
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		list = null;
+	}
+
+	@Test
+	public void testEmpty() {
+		final Iterator<Integer> i = list.iterator();
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void testNonempty() {
+		list.add(33);
+		list.add(77);
+		list.add(44);
+		list.add(77);
+		list.add(55);
+		list.add(77);
+		list.add(66);
+		final Iterator<Integer> i = list.iterator();
+		assertTrue(i.hasNext());
+		assertEquals(new Integer(33), i.next());
+		// TODO fix the expected values in the assertions below
+		assertTrue(i.hasNext());
+		assertEquals(77, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(44, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(77, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(55, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(77, i.next().intValue());
+		assertTrue(i.hasNext());
+		assertEquals(66, i.next().intValue());
+		assertFalse(i.hasNext());
+	}
+
+	@Test
+	public void testRemove() {
+		list.add(33);
+		list.add(77);
+		list.add(44);
+		list.add(77);
+		list.add(55);
+		list.add(77);
+		list.add(66);
+		final Iterator<Integer> i = list.iterator();
+		while (i.hasNext()) {
+			if (i.next() == 77) {
+				i.remove(); // TODO what happens if you use list.remove(77)?
+			}
+		}
+		// TODO using assertEquals and Arrays.asList (see above)
+		// express which values are left in the list
+		Integer [] testList = {33,44,55,66};
+		assertEquals(Arrays.asList(testList), list);
+	}
+
+	@Test
+	public void testAverageValues() {
+		list.add(33);
+		list.add(77);
+		list.add(44);
+		list.add(77);
+		list.add(55);
+		list.add(77);
+		list.add(66);
+		double sum = 0;
+		int n = 0;
+		// TODO use an iterator and a while loop to compute the average (mean) of the values
+		// (defined as the sum of the items divided by the number of items)
+		final Iterator<Integer> i = list.iterator();
+		while (i.hasNext() && n<list.size()){
+			sum = sum + list.get(n);
+			n++;
+		}
+		assertEquals(61, (int)(sum / n));
+		assertEquals(7, n);
+	}
+}
